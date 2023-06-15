67928597a36c9de4d371cf1b07c9cfe18d5a2510
elvinsavio <elvinsavio.lp@gmail.com>
2023-06-15 14:44:46 +0530

    rewrite with es6

---
+++ src/app/model/model.ts
@@ -0,0 +1,14 @@
+import dbConfig from "../../config/db.config";
+import { Dialect, Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect as Dialect,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
