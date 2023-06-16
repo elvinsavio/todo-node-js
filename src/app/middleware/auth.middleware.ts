06a3d8d2190e31b9338d0707405be949c6d1d4c3
elvinsavio <elvinsavio.lp@gmail.com>
2023-06-16 13:53:03 +0530

    add jwt to routes

--- src/app/middleware/auth.middleware.ts
+++ src/app/middleware/auth.middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 import error from "../templates/error";
+import jwt from "../utils/jwt";
+import response from "../templates/response";
 
 export default {
   signin(req: Request, res: Response, next: NextFunction) {
@@ -10,5 +12,22 @@ export default {
       return res.status(400).send(error);
     }
     return next();
+  },
+
+  async verifyToken(req: Request, res: Response, next: NextFunction) {
+    // check if token is there
+    if (!req.headers.authorization) {
+      error.errorMessage = "Missing authorization header";
+      return res.status(401).send(error);
+    }
+    jwt
+      .verify(req.headers.authorization)
+      .then(() => {
+        return next();
+      })
+      .catch((err) => {
+        error.errorMessage = err.message;
+        res.status(401).send(error);
+      });
   },
 };
--- src/app/router/user.router.ts
+++ src/app/router/user.router.ts
@@ -1,14 +1,38 @@
 import { Router } from "express";
 import userController from "../controller/user.controller";
 import userMiddleware from "../middleware/user.middleware";
+import authMiddleware from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
+/**
+ * @description
+ * Get all users
+ */
+router.get("/", authMiddleware.verifyToken, userController.getAllUsers);
 
+/**
+ * @description
+ * Get user by id
+ */
+router.get("/:id", authMiddleware.verifyToken, userController.getUserById);
+
+/**
+ * @description
+ * Create user
+ */
 router.post("/", userMiddleware.createUser, userController.createUser);
-router.post("/:id", userMiddleware.updateUser, userController.updateUser);
-router.post("/toggleState/:id", userMiddleware.toggleActive, userController.toggleUser);
+
+/**
+ * @description
+ * Update user
+ */
+router.post("/:id", authMiddleware.verifyToken, userMiddleware.updateUser, userController.updateUser);
+
+/**
+ * @description
+ * Toggle user active
+ */
+router.post("/toggleState/:id", authMiddleware.verifyToken, userMiddleware.toggleActive, userController.toggleUser);
 
 export default router;
